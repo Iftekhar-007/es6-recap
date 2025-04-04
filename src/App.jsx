8961ad0987263c173b19ff9e6411cd691e09eb21
Iftekhar-007 <shawonsp534@gmail.com>
2025-04-04 19:59:10 +0600

    first open

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import add from './utils/fnc'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const sum = add(5,6)
+  console.log(sum); 
 
   return (
     <>
